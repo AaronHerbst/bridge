167bfba27c4794d7e3da4c6ef4a0ade86051267d
konicekc <konicekc@msoe.edu>
2024-03-08 16:17:15 -0600

    implemented generatePrompt logic

--- src/bridge/Bridge.java
+++ src/bridge/Bridge.java
@@ -8,18 +8,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import javax.net.ssl.*;
-
-import java.io.IOException;
+import javax.net.ssl.SSLContext;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+
+
+
+import java.io.IOException;
+
 
 import java.util.ArrayList;
 import java.util.Base64;
@@ -34,7 +35,25 @@ public class Bridge extends Application {
     private static String encoding;
     private static String url;
     private static HttpClient client;
+    static class TrustAllManager {
+        public static javax.net.ssl.TrustManager[] getTrustManagers() {
+            return new javax.net.ssl.TrustManager[] {
+                    new javax.net.ssl.X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
 
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] certs,
+                                                       String authType) {
+                        }
+
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] certs,
+                                                       String authType) {
+                        }
+                    }
+            };
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
@@ -58,9 +77,9 @@ public class Bridge extends Application {
         role = roll;
 
         // Disable SSL certificate check
+
         SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(null, TrustAllManager.getTrustManagers(),
-                new java.security.SecureRandom());
+        sslContext.init(null, TrustAllManager.getTrustManagers(), new java.security.SecureRandom());
         encoding = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
 
         // Create a custom HttpClient with the disabled certificate check
@@ -307,7 +326,7 @@ public class Bridge extends Application {
         log(prompt);
         for (int i = 0; i < 3; i++) {
             HttpRequest request = HttpRequest.newBuilder()
-                    //there should be a getLog() call here, along with necessary syntax
+                    //there should be a getLog() call here, along with nessacary syntax
                     .uri(URI.create(url+prompt))
                     .setHeader("Authorization", "Basic " + encoding).build();
             HttpResponse<String> response = client.send(request,
